ae33b9c5c38c41dbe8a0c6ffa06e4a23520ca73b
talhaalime <talhaalime@gmail.com>
2025-07-13 15:40:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -376,4 +376,6 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=5005, reload=True)
+    
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    
